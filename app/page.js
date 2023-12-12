0560ecf61367975a4ff779fb1c978ad4e201e55e
Chien Wei Chou <chien.w.chou@gmail.com>
2023-12-12 15:59:10 -0800

    create take quiz component

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Footer from "./components/footer";
 import Header from "./components/header";
 import Homepage from "./components/homepage";
+import TakeQuiz from "./components/takeQuiz";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Homepage />
+      {/* <Homepage /> */}
+      <TakeQuiz />
       <Footer />
     </>
   )
